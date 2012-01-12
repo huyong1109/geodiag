6d768cb96baed6efaee004b56cfa59e22e6a320b
Li Dong <dongli@lasg.iap.ac.cn>
2012-01-12 11:33:22 +0800

    Add NCL utils.

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# ------------------------------------------------------------------------------
+# Description:
+#
+#   This script is used to setup some environment variables or other things to
+#   facilitate the running of GEODIAG.
+#
+# Authors:
+#
+#   Li Dong
+# ------------------------------------------------------------------------------
+
+export GEODIAG_ROOT=$(dirname $BASH_ARGV)
