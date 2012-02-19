bb4973afc279d848a9cf40190998a09812141c00
Li Dong <dongli@lasg.iap.ac.cn>
2012-02-19 10:22:30 +0800

    Add subcommand autocomplete

--- setup.sh
+++ setup.sh
@@ -18,3 +18,22 @@ export PATH=$PATH:$GEODIAG_ROOT
 for setup_script in $(find $GEODIAG_ROOT/scripts -name setup.sh); do
     source $setup_script
 done
+
+# command line completion
+function _geodiag_()
+{
+    local prev_argv=${COMP_WORDS[COMP_CWORD-1]}
+    local curr_argv=${COMP_WORDS[COMP_CWORD]}
+    completed_words=""
+    if [[ "${prev_argv##*/}" == "geodiag" ]]; then
+        # complete subcommands
+        case "$curr_argv" in
+        "")
+            completed_words="update"
+            ;;
+        esac
+    fi
+    COMPREPLY=($(compgen -W "$completed_words" -- $curr_argv))
+}
+
+complete -o default -F _geodiag_ geodiag
