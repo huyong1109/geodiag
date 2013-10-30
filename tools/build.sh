e3de64245d896a22378f85e6642fb8002b8bb164
Li Dong <dongli@lasg.iap.ac.cn>
2013-10-30 19:54:01 +0800

    Add commands to create shared dir if not exist

--- tools/build.sh
+++ tools/build.sh
@@ -1,5 +1,9 @@
 DIRS=$GEODIAG_TOOLS/eemd
 
+if [[ ! -d $GEODIAG_TOOLS/shared ]]; then
+    mkdir $GEODIAG_TOOLS/shared
+fi
+
 cd $GEODIAG_TOOLS/shared
 for DIR in $DIRS; do
     res=$(make -f $DIR/Makefile)
